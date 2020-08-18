afccd86aa1ca145439eb66b567d001d124f70082
Seongmo Hyeong <237ref@gmail.com>
2020-08-19 08:19:31 +0900

    [C#] DataTime Format

---
+++ DataTimeFormat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using static System.Console;
+
+namespace Library
+{
+    public class DataTimeFormat
+    {
+        static void Main(string[] args)
+        {
+            DataTime dt = new DataTime(2020, 08, 19, 07, 23, 07);
+            
+            WriteLine("{0:yyyy-MM-dd HH:mm:ss (dddd)}", dt);
+            CultureInfo ciKo = new CultureInfo("ko-KR);
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciKo));
+        }
+    }
+}
