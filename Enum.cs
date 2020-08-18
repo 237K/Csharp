08035bfdf73946e7f7e683fe6261942438f2d64b
237K <237ref@gmail.com>
2020-08-18 21:09:19 +0900

    [C#] Enum

---
+++ Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace Enum
+{
+	public class Enum
+	{
+		enum DialogResult {YES, NO, CANCEL, CONFIRM, OK}
+		static void Main(string[] args)
+        {
+			DialogResult result = DialogResult.YES;
+			WriteLine(result == DialogResult.YES);
+			WriteLine(result == DialogResult.NO);
+			WriteLine(result == DialogResult.CANCEL);
+			WriteLine(result == DialogResult.CONFIRM);
+			WriteLine(result == DialogResult.OK);
+        }
+	}
+}
