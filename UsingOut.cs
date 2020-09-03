74973ef832f4fd12436f3e3a4bf254e9cfe2c479
237K <237ref@gmail.com>
2020-09-03 15:55:58 +0900

    [C#] 출력 전용 매개 변수(다수의 값 return할 때) out 사용법을 공부했습니다.

---
+++ UsingOut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UsingOut
+{
+	public class MainApp
+	{
+		static void Divide(int a, int b, out int quo, out int rmd)
+        {
+			quo = a / b;
+			rmd = a % b;
+			return;
+        }
+
+		static void Main(string[] args)
+        {
+			int a = 20;
+			int b = 3;
+			Divide(a, b, out int c, out int d);
+			Console.WriteLine($"a:{a}, b:{b}, a/b:{c}, a%b:{d}");
+        }
+	}
+}
